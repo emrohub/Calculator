811725598cc74286ecd03b814db0a4182db9f7e9
emrohub <emmyrosen234@gmail.com>
2023-11-05 18:13:58 +0100

    Update calculatorGUI.py

--- calculator/calculatorGUI.py
+++ calculator/calculatorGUI.py
@@ -5,7 +5,7 @@ from tkinter.constants import SUNKEN
 
 window = tk.Tk()
 window.title('Calculator')
-frame = tk.Frame(master = window, bg = "green", padx = 10)
+frame = tk.Frame(master = window, bg = "lavender", padx = 10)
 frame.pack()
 entry = tk.Entry(master=frame, relief= SUNKEN, borderwidth=3, width=30)
 entry.grid(row=0, column=0, columnspan=3, ipady=2, pady=2)
